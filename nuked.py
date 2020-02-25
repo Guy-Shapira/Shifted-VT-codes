a8c55e24f44bf561388080ede9c9db63290250a1
Guy Shapira <Guy.shapira@campus.technion.ac.il>
2020-02-25 20:02:24 +0200

    NUKED

---
+++ nuked.py
@@ -0,0 +1,59 @@
+from itertools import product
+from typing import List
+from random import randrange
+
+
+#TODO: remove 0 from the start somewhere
+
+def gen(c, d, n, P):
+    assert P > 1
+    for tup in product([1, 0], repeat=n):
+        tup = list(tup)
+        if is_legal_vector(tup, c, d, P):
+            start_index = randrange(n)
+            max_error_index = min(n-1, start_index + P - 2)
+            delete_index = randrange(start_index, max_error_index+1)
+            fucked_tup = tup.copy()
+            fucked_tup.pop(delete_index)
+            saved_fuck_up = fucked_tup.copy()
+            rec = reconstruct(fucked_tup, d, c, P, start_index, delete_index)
+            if not rec == tup:
+                print("----")
+                print(tup)
+                print(saved_fuck_up)
+                print(rec)
+                reconstruct(saved_fuck_up, d, c, P, start_index, delete_index)
+
+def is_legal_vector(vector: List[int], c: int, d: int, P: int) -> bool:
+    return weighted_sum(vector) % P == c and sum(vector) % 2 == d
+
+
+def weighted_sum(vector: List[int]) -> int:
+    return sum([(i+1) * x for i, x in zip(range(0, len(vector)), vector)])
+
+
+def sum_mod2(vector: List[int]) -> int:
+    return sum(vector) % 2
+
+
+def reconstruct(vector: List[int], d: int, c: int, P: int, index: int, realIndex) -> List[int]:
+    missing_value = 0 if sum_mod2(vector) == d else 1
+    start = index
+
+    end = min(len(vector), index + P - 2)
+
+    # handle delete of 1
+    errd_vector = vector[index: end + 1]
+
+
+
+    if missing_value == 1:
+        return 1/0
+        zero_count_to_the_right = count()
+
+
+
+
+if __name__ == "__main__":
+    gen(c=2, d=1, n=5, P=2)
+    print("done")
