6c87be0620aa6c9f27854718f641f87fc64a0b48
Maxim Barsky <maximb99@live.com>
2020-03-10 13:45:45 +0200

    wrote basic unit tests.

--- test_insertion_deletion.py
+++ test_insertion_deletion.py
@@ -6,7 +6,7 @@ from deletions_reconstruction import reconstruct_deletion
 from utils import is_legal_vector
 import unittest
 
-
+# TODO: consider changing these from global parameters to class parameters or something
 n = 18
 c = 6
 d = 1
